2039c1a7a2daec9cac81a83e253b1ab394e822a9
PYJ <dudwls305@gmail.com>
2022-06-20 23:20:33 +0900

    commit

--- app/src/main/java/com/pyj/ylife/view/menu/MenuMap.java
+++ app/src/main/java/com/pyj/ylife/view/menu/MenuMap.java
@@ -122,37 +122,40 @@ public class MenuMap extends Fragment implements
         List<Address> addressList = null;
         String str=editText.getText().toString();
 
+        // editText에 입력한 텍스트(주소, 지역, 장소 등)을 지오 코딩을 이용해 변환
         try {
-            // editText에 입력한 텍스트(주소, 지역, 장소 등)을 지오 코딩을 이용해 변환
             addressList = geocoder.getFromLocationName(
                     str, // 주소
                     10); // 최대 검색 결과 개수
+            if (addressList.size() == 0){   //edittext를 잘 못 입력한 경우
+                Toast.makeText(getContext(), "다시 입력하세요", Toast.LENGTH_SHORT).show();  //오류 발생 토스트 알림
+                throw new Exception();                                                         // 예외처리로 이동
+            } else {
+                System.out.println(addressList.get(0).toString());
+                // 콤마를 기준으로 split
+                String[] splitStr = addressList.get(0).toString().split(",");
+                String address = splitStr[0].substring(splitStr[0].indexOf("\"") + 1, splitStr[0].length() - 2); // 주소
+                System.out.println(address);
+
+                String latitude = splitStr[10].substring(splitStr[10].indexOf("=") + 1); // 위도
+                String longitude = splitStr[12].substring(splitStr[12].indexOf("=") + 1); // 경도
+                System.out.println(latitude);
+                System.out.println(longitude);
+
+                // 좌표(위도, 경도) 생성
+                LatLng point = new LatLng(Double.parseDouble(latitude), Double.parseDouble(longitude));
+                // 마커 생성
+                MarkerOptions mOptions2 = new MarkerOptions();
+                mOptions2.title("search result");
+                mOptions2.snippet(address);
+                mOptions2.position(point);
+                // 마커 추가
+                map.addMarker(mOptions2);
+                // 해당 좌표로 화면 줌
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(point, 15));
+            }
+        } catch (Exception e) {  //오류발생시 예외처리
+            System.out.println(e);
         }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println(addressList.get(0).toString());
-        // 콤마를 기준으로 split
-        String []splitStr = addressList.get(0).toString().split(",");
-        String address = splitStr[0].substring(splitStr[0].indexOf("\"") + 1,splitStr[0].length() - 2); // 주소
-        System.out.println(address);
-
-        String latitude = splitStr[10].substring(splitStr[10].indexOf("=") + 1); // 위도
-        String longitude = splitStr[12].substring(splitStr[12].indexOf("=") + 1); // 경도
-        System.out.println(latitude);
-        System.out.println(longitude);
-
-        // 좌표(위도, 경도) 생성
-        LatLng point = new LatLng(Double.parseDouble(latitude), Double.parseDouble(longitude));
-        // 마커 생성
-        MarkerOptions mOptions2 = new MarkerOptions();
-        mOptions2.title("search result");
-        mOptions2.snippet(address);
-        mOptions2.position(point);
-        // 마커 추가
-        map.addMarker(mOptions2);
-        // 해당 좌표로 화면 줌
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(point,15));
     }
 }
